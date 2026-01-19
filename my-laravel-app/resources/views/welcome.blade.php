9472316bbddd6addd230e3aad48af469388fad0a
Emīls Šīre <166484726+23DP4ESire@users.noreply.github.com>
2026-01-19 07:50:06 +0000

    homepage ideja

--- my-laravel-app/resources/views/welcome.blade.php
+++ my-laravel-app/resources/views/welcome.blade.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 12 with Vue 3</title>
-    @vite(['resources/js/app.js'])
+    <title>Laravel</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <div id="app"></div>
+<body class="antialiased bg-gray-100">
+    <div class="min-h-screen flex items-center justify-center">
+        <h1 class="text-3xl font-bold text-red-500">
+            Laravel + Vite is working 🚀
+        </h1>
+    </div>
 </body>
 </html>
